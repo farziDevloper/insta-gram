24fa15bc5d3274ca6978cb8c609e6e724bc42afe
Abhishek <abhishek@imoneypay.in>
2024-02-07 19:51:05 +0530

    global css , modular css, custom layout , nested layout , conditional layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,19 @@
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return <main className={styles.main}>Hello World</main>;
+  return (
+    <main className={styles.main}>
+      Hello World
+      <div>
+        <Link href="/admin/dashboard">Go to admin dashboard</Link>
+        <div>
+          <Link href={"/product/122"}>Go to product page</Link>
+        </div>
+        <div>
+          <Link href={"/product/122/review/111"}>Go to product page</Link>
+        </div>
+      </div>
+    </main>
+  );
 }
